5fcba2d90aa873acb113404a6b83440ee81054ec
Fabian Garcia <112596541+fabigar6@users.noreply.github.com>
2025-12-31 08:43:43 -0300

    cambie el pilar a cono de trafico

--- src/limo_patrol/limo_patrol/obstacle_spawner.py
+++ src/limo_patrol/limo_patrol/obstacle_spawner.py
@@ -9,28 +9,30 @@ Y_MIN, Y_MAX = -4.0, 4.0
 ROBOT_SAFE_RADIUS = 1.0
 
 
-BOX_SDF = """
+CONE_SDF = """
 <sdf version="1.6">
   <model name="{name}">
     <static>true</static>
     <link name="link">
       <collision name="collision">
         <geometry>
-          <box>
-            <size>0.8 0.8 0.8</size>
-          </box>
+          <cone>
+            <radius>0.25</radius>
+            <length>0.7</length>
+          </cone>
         </geometry>
       </collision>
       <visual name="visual">
         <geometry>
-          <box>
-            <size>0.8 0.8 0.8</size>
-          </box>
-          <material>
-            <ambient>0.8 0.2 0.2 1</ambient>
-            <diffuse>0.8 0.2 0.2 1</diffuse>
-          </material>
+          <cone>
+            <radius>0.25</radius>
+            <length>0.7</length>
+          </cone>
         </geometry>
+        <material>
+          <ambient>1 0.5 0 1</ambient>
+          <diffuse>1 0.5 0 1</diffuse>
+        </material>
       </visual>
     </link>
   </model>
@@ -38,8 +40,8 @@ BOX_SDF = """
 """
 
 
-def spawn_box(name, x, y):
-    sdf_content = BOX_SDF.format(name=name)
+def spawn_cone(name, x, y):
+    sdf_content = CONE_SDF.format(name=name)
 
     with tempfile.NamedTemporaryFile(delete=False, suffix='.sdf') as f:
         f.write(sdf_content.encode('utf-8'))
@@ -51,14 +53,14 @@ def spawn_box(name, x, y):
         '-file', sdf_path,
         '-x', str(x),
         '-y', str(y),
-        '-z', '0.4'
+        '-z', '0.35'  # mitad de la altura del cono
     ])
 
     os.remove(sdf_path)
 
 
 def main():
-    print('[Spawner] Generating random obstacles...')
+    print('[Spawner] Generando conos de trafico aleatorios...')
 
     num = 4
     i = 0
@@ -67,15 +69,15 @@ def main():
         x = random.uniform(X_MIN, X_MAX)
         y = random.uniform(Y_MIN, Y_MAX)
 
-        # evitar spawnear cerca del robot
         if (x**2 + y**2)**0.5 < ROBOT_SAFE_RADIUS:
             continue
 
-        spawn_box(f'obstacle_{i}', x, y)
+        spawn_cone(f'cone_{i}', x, y)
         i += 1
 
-    print('[Spawner] Obstacles spawned')
+    print('[Spawner] Conos de trafico creados')
 
 
 if __name__ == '__main__':
     main()
+
